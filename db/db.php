43caf9adec60ca337c761dd16cc18b4e428b366f
zaidun <zaidun10@yahoo.com>
2020-09-12 17:02:59 +0200

    fds

--- db/db.php
+++ db/db.php
@@ -17,7 +17,7 @@
     article_titre varchar(120) NOT NULL,
     article_contenu text NOT NULL,
     article_date timestamp WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP ,
-    article_image_data blob DEFAULT NULL,
+    article_image_data BYTEA DEFAULT NULL,
     article_image_type varchar(40) DEFAULT NULL,
     user_id INT DEFAULT NULL,
     categorie_nom varchar(30) DEFAULT NULL
