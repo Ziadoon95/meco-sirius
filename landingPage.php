17b33276fefbe1a73aa3b54140c814235a7a862b
zaidun <zaidun10@yahoo.com>
2020-09-12 14:50:19 +0200

    start

---
+++ landingPage.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Média Collaboratif</title>
+    <link rel="stylesheet" href="assets/css/style-mobile.css" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;600;700&display=swap"
+      rel="stylesheet"
+    />
+  </head>
+  <body>
+    <div class="container">
+      <div class="container__mainContent">
+        <div class="container__mainContent--header">
+          <div class="container__mainContent--logo">
+            <img src="./assets/images/logo.svg" alt=""> 
+            <h4>Lorem ipsum dolor sit amet</h4>
+          </div>
+          <div class="container__mainContent--text">
+            <a class="inscription" href="./client/inscription.php">S'inscrire</a>
+            <a class="connexion" href="./client/connexionLoginInterface.php">Se connecter</a>
+            <a class="continuerSansCompte" href="landing.html">
+              Continuer sans compte
+            </a>
+          </div>
+        </div>
+        <div class="container__mainContent--destop">
+          <div class="container__mainContent__destop--block1">
+            <div class="container__mainContent__destop__block1--text">
+              <h2>
+                Enrichir vos connaissances,
+                <br />
+                révéler votre potentiel.
+              </h2>
+              <p>
+                Rejoignez dès à présent notre communauté de curieux ! Vous
+                souhaitez lire des articles en tous genres et de qualité ? Vous
+                souhaitez partager votre expériences et rédiger des articles
+                pour la communauté ? Vous êtes au bon endroit.
+              </p>
+              <div class="button">
+                <a
+                  class="inscription"
+                  href="./client/inscription.php"
+                >
+                  S'inscrire
+                </a>
+                <a
+                  class="connexion"
+                  href="./client/connexionLoginInterface.php"
+                >
+                  Se connecter
+                </a>
+              </div>
+            </div>
+            <div class="container__mainContent__destop__block1--image">
+              <img src="./assets/images/Landing-image.png" alt="" />
+            </div>
+          </div>
+          <h4 class="tirezProfit">
+            Tirez profit de ce que MECO a à vous offrir
+          </h4>
+          <div class="container__mainContent__destop--block2">
+            <div class="troisColonnes">
+              <div class="cercle">
+                <img src="./assets/images/icone article 1.png" />
+              </div>
+              <p>
+                Une multitude d’articles
+                <br />
+                de qualité
+              </p>
+            </div>
+            <div class="troisColonnes">
+              <div class="cercle">
+                <img src="./assets/images/Icone communauté.png" />
+              </div>
+              <p>
+                Une communauté engagée
+                <br />
+                de lecteurs et d’écrivains
+              </p>
+            </div>
+            <div class="troisColonnes">
+              <div class="cercle">
+                <img src="./assets/images/Icone 3.png" />
+              </div>
+              <p>
+                C’est gratuit et sans
+                <br />
+                publicités !
+              </p>
+            </div>
+          </div>
+          <div class="container__mainContent__destop--block3">
+            <h4>
+              Découvez MECO dès maintenant et écrivez votre premier article !
+            </h4>
+            <div class="bottomButton">
+              <a
+                class="inscription"
+                href="./client/inscription.php"
+              >
+                S'inscrire
+              </a>
+              <a
+                class="continuerSansCompte"
+                href="./homepage.php"
+              >
+                Continuer sans compte
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
