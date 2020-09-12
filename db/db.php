2af90a3b3f926733a5ab92359d741cfa6b55d488
zaidun <zaidun10@yahoo.com>
2020-09-12 16:17:39 +0200

    u

--- db/db.php
+++ db/db.php
@@ -11,6 +11,30 @@
     $conn =new PDO('pgsql:host=ec2-46-137-124-19.eu-west-1.compute.amazonaws.com;port=5432;dbname=d8bj6eaum2o8hi', $sqlUserName, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql = "CREATE TABLE IF NOT EXISTS `articles` (
+     `article_id` int(11) NOT NULL AUTO_INCREMENT  PRIMARY KEY,
+    `article_titre` varchar(120) NOT NULL,
+    `article_contenu` text NOT NULL,
+    `article_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+    `article_image_data` blob DEFAULT NULL,
+    `article_image_type` varchar(40) DEFAULT NULL,
+    `user_id` int(11) DEFAULT NULL,
+    `categorie_nom` varchar(30) DEFAULT NULL
+)";
+$conn->exec($sql);
+      $sql_two ="CREATE TABLE IF NOT EXISTS `utilisateurs` (
+        `user_id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        `user_prenom` varchar(64) COLLATE utf8_bin NOT NULL,
+        `user_nom` varchar(64) COLLATE utf8_bin NOT NULL,
+        `user_email` varchar(64) COLLATE utf8_bin NOT NULL,
+        `user_mdp` varchar(250) COLLATE utf8_bin NOT NULL,
+        `user_image` mediumblob DEFAULT NULL,
+        `user_is_connected` int(1) DEFAULT NULL
+      )";    
+      
+      $conn->exec($sql_two);
+
   } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
   }
