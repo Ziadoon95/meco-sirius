7852737e586a8dfdd1a8c994b3782d3f5ba637a2
zaidun <zaidun10@yahoo.com>
2020-09-12 16:03:20 +0200

    update

--- db/db.php
+++ db/db.php
@@ -1,10 +1,14 @@
  <?php
-  $servername = "localhost";
+/*   $servername = "localhost";
   $sqlUserName = "root";
-  $password = "";
-
+  $password = ""; */
+  $servername = "ec2-46-137-124-19.eu-west-1.compute.amazonaws.com";
+  $sqlUserName = "plahhygjpajudg";
+  $password = "fcd915d7540cac284cb31ce5c8c8b09c55e72593edaea9d62c6a766a623127a7";
+  
   try {
-    $conn = new PDO("mysql:host=$servername;dbname=meco", $sqlUserName, $password);
+    //$conn = new PDO("mysql:host=$servername;dbname=meco", $sqlUserName, $password);
+    $conn =new PDO('pgsql:host=$servername;port=5432;dbname=d8bj6eaum2o8hi', $sqlUserName, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   } catch (PDOException $e) {
